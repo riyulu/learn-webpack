909e8b891ea7d6ca0f47c2e0abc0182c79a54c40
lu riyu <v_rylu@tencent.com>
2019-11-14 19:39:32 +0800

    test: webpack.base test

--- test/unit/webpack-base-test.js
+++ test/unit/webpack-base-test.js
@@ -2,10 +2,9 @@ const assert = require('assert');
 const baseConfig = require('../../lib/webpack.base');
 
 describe('webpack.base.js test case', () => { // eslint-disable-line
-  console.log(baseConfig); // eslint-disable-line
 
   it('entry', () => { // eslint-disable-line
-    assert.equal(baseConfig.entry.index, 'E:/learn/webpack_learn/builder-webpack/test/smoke/template/src/index/index.js');
-    assert.equal(baseConfig.entry.search, 'E:/learn/webpack_learn/builder-webpack/test/smoke/template/src/search/index.js');
+    assert.equal(baseConfig.entry.index.indexOf('learn-webpack/test/smoke/template/src/index/index.js') > -1, true);
+    assert.equal(baseConfig.entry.search.indexOf('learn-webpack/test/smoke/template/src/search/index.js') > -1, true);
   });
 });
